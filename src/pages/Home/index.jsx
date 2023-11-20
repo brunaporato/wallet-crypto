9ea966ab1d34f1779af93c1841c979b1c1018d57
Bruna <brunaporato@gmail.com>
2023-11-20 13:18:45 -0300

    add home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components/Navbar";
+import './styles.scss';
+
+export function Home() {
+  return (
+    <div className="home-container">
+      <Navbar />
+    </div>
+  )
+}

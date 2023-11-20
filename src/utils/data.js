0a73775bb7c61005ecb36331b0ab06b5d0689504
Bruna <brunaporato@gmail.com>
2023-11-20 17:19:39 -0300

    set data

---
+++ src/utils/data.js
@@ -0,0 +1,34 @@
+export const dataList = [
+  {
+    title: 'day',
+    lower: '3.690',
+    higher: '3.772',
+    btcToDollar: '3.867',
+    areaStroke: '#ff8f17',
+    areaFill: '#ffead2',
+  },
+  {
+    title: 'week',
+    lower: '3.350',
+    higher: '4.782',
+    btcToDollar: '3.867',
+    areaStroke: '#ffba39',
+    areaFill: '#ffead2',
+  },
+  {
+    title: 'month',
+    lower: '3.150',
+    higher: '4.952',
+    btcToDollar: '3.867',
+    areaStroke: '#ff981e',
+    areaFill: '#ffead2',
+  },
+  {
+    title: 'year',
+    lower: '3.215',
+    higher: '5.852',
+    btcToDollar: '3.867',
+    areaStroke: '#ffc03d',
+    areaFill: '#ffead2',
+  },
+]
